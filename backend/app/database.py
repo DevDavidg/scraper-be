8135f50505ed33ffe8ac3091700ff94d89dce589
David <dev.davidg@gmail.com>
2025-01-19 00:40:52 -0300

    update mongo database

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 uri = os.getenv("MONGO_URI")
 
-client = MongoClient(uri)
+client = MongoClient(uri, tls=True) 
 
 try:
     client.admin.command("ping")
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+    print(f"Error connecting to MongoDB: {e}")
 
-db = client["cluster_data"]  
-scraped_data_collection = db["scraped_data"]  
+db = client["cluster_data"]
+scraped_data_collection = db["scraped_data"]
